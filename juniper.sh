b342738fa1f78ad1b2cb62fc149ba99590e4f142
cweigmann <cweigmann@localhost.localdomain>
2014-05-25 16:29:18 +0200

    make scrip more robust - no one reads scripts

--- juniper.sh
+++ juniper.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+host2connect=dummy
+port2connect=0
 if [[ $@ =~ "--help" || $# -eq 0 ]]; then
 echo -e "run with \njuniper.sh start or \n\
 juniper.sh start -u/--username foo or \n\
@@ -41,6 +43,10 @@ cd /usr/local/nc
 if [[ $start_stop = "stop" ]]; then
         ./ncsvc -K
 else
+	if [[ $port2connect -eq 0 || $host2connect == "dummy" ]]; then
+		echo "$host2connect:$port2connect will not work - promised. Start with -h/--host and -p/--port or change script"
+		exit 1
+	fi
         VPN_USER=${VPN_USER:-$USER}
 
         openssl s_client -connect $host2connect":"$port2connect <<<"" 2>&1 | sed -ne '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' | openssl x509 -out ~/cert.der -outform der
